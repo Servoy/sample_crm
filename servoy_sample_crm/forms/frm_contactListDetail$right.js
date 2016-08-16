38f8f7255c514fa57312857f8924c29ea42854d1
R. van Veen <rvanveen@servoy.com>
2016-08-16 08:30:34 +0200

    - Added the correct dataprovider to the fields

--- servoy_sample_crm/forms/frm_contactListDetail$right.js
+++ servoy_sample_crm/forms/frm_contactListDetail$right.js
@@ -21,47 +21,6 @@ function deleteContact() {
 		foundset.deleteRecord(rec);
 	}
 }
-/**
- *
- * @param {JSEvent} event the event that triggered the action
- *
- * @protected
- *
- * @properties={typeid:24,uuid:"71A99044-2F6F-4A8A-AD00-4D5CBF35FB6C"}
- */
-function uploadImage(event) {
-	plugins.file.showFileOpenDialog(1, null, false, new Array("JPG and GIF", "jpg", "gif"), saveImage);
-}
-
-/**
- * @private
- * @param {Array<plugins.file.JSFile>} selectedImage
- *
- * @properties={typeid:24,uuid:"EE329CB9-7534-44A4-965A-2E84AA5720FB"}
- */
-function saveImage(selectedImage) {
-	var file = plugins.images.getImage(selectedImage[0].getBytes())
-	if (file) {
-		image = scopes.contactUtils.resizeImageRatio(file).getData()
-	}
-}
-/**
- *
- * @param {String} oldValue
- * @param {String} newValue
- * @param {JSEvent} event the event that triggered the action
- *
- * @private
- *
- * @properties={typeid:24,uuid:"4FD69E28-87CC-4500-9F64-1B90F9A0E4CE"}
- */
-function editContactName(oldValue, newValue, event) {
-	var obj = scopes.contactUtils.splitContactFullName(c_name_full)
-	name_first = obj.firstName;
-	name_last = obj.lastName;
-	name_middle = obj.middleName;
-	return true
-}
 
 /**
  *
